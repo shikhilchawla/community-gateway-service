b72502c170dfa954cf4a4f529a3340c17808fe35
shikhilchawla <shikhilchawla@gmail.com>
2024-11-26 22:33:15 -0600

    added routes to create community, groups, posts, comments

---
+++ src/main/java/org/phoenix/apps/community/gateway/CommunityGatewayService.java
@@ -0,0 +1,11 @@
+package org.phoenix.apps.community.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityGatewayService {
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityGatewayService.class, args);
+    }
+}
